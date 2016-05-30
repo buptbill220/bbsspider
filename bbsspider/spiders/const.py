645aa377dff4e35ea0141d94471d37c622474497
buptbill220 <buptbill220@163.com>
2016-05-30 21:15:22 +0800

    delete passwd

--- bbsspider/spiders/const.py
+++ bbsspider/spiders/const.py
@@ -3,5 +3,5 @@
 URL = 'http://bbs.byr.cn';
 ALLOW_DOMAINS = ['bbs.byr.cn'];
 HEADERS = {'User-Agent': 'Mozilla/5.0', 'Host': 'bbs.byr.cn', 'X-Requested-With': 'XMLHttpRequest', 'Connection': 'keep-alive'};
-LOGIN_DATA = {'id': 'bill220', 'passwd': 'daxiefangming', 'mode' : '0', 'CookieDate' : '0'};
+LOGIN_DATA = {'id': 'bill220', 'passwd': 'xxxxxxxx', 'mode' : '0', 'CookieDate' : '0'};
 DB_CONFIG = {'host': '127.0.0.1', 'user':'root', 'passwd':'', 'db':'bupt', 'port': 3306, 'charset': 'utf8', 'timeout': 5};
